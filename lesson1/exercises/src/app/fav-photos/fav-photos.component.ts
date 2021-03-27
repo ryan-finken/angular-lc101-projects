b9a6a881e30e5b573d5bb49d389dedfdc08cbedd
Ryan Finken <ryanfinken338@gmail.com>
2021-03-26 21:40:10 -0500

    saving before moving on to 28.7.3.Part2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'A Better Section Heading For Some Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://m.media-amazon.com/images/M/MV5BMTkyNjA5NTA1NV5BMl5BanBnXkFtZTcwNjU0NjgyMQ@@._V1_UY317_CR3,0,214,317_AL_.jpg';
+  image3 = 'https://www.sierraclub.org/sites/www.sierraclub.org/themes/pt/images/logos/sc-logo-green.svg';
 
   constructor() { }
 
