22818544f22a390601dbf9a37f0c28368a25ae78
Ryan Finken <ryanfinken338@gmail.com>
2021-03-27 13:11:41 -0500

    finished main assignment, will do 28.7.4.1 later

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'A Better Section Heading For Some Photos';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+  photosTitle = 'A Few of My Favorite Things';
+  image1 = 'https://static.wixstatic.com/media/8d3f7b_a93686d900924aa7afe978d7e0b309de~mv2.jpg/v1/fill/w_739,h_381,al_c,lg_1,q_80/8d3f7b_a93686d900924aa7afe978d7e0b309de~mv2.webp';
   image2 = 'https://m.media-amazon.com/images/M/MV5BMTkyNjA5NTA1NV5BMl5BanBnXkFtZTcwNjU0NjgyMQ@@._V1_UY317_CR3,0,214,317_AL_.jpg';
   image3 = 'https://www.sierraclub.org/sites/www.sierraclub.org/themes/pt/images/logos/sc-logo-green.svg';
 
