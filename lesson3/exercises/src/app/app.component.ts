5f66640fdf52173da851fe199e155fc0130a3215
Ryan Finken <ryanfinken338@gmail.com>
2021-04-03 15:42:26 -0500

    will try rocket mover thingy later

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,45 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  takeoff(): void {
+    const launch: boolean = confirm('Confirm that the shuttle is ready for takeoff.');
+    if (launch) {
+      this.message = 'Shuttle in flight.';
+      this.color = 'blue';
+      this.height += 10000;
+    }
+  }
+
+  land(): void {
+    alert('The shuttle is landing. Landing gear engaged.');
+    this.message = 'The shuttle has landed.';
+    this.color = 'green';
+    this.height = 0;
+  }
+
+  abort(): void {
+    const missionAbort: boolean = confirm('Do you really want to abort the mission?');
+    if (missionAbort) {
+      this.message = 'Mission aborted.';
+      this.color = 'red';
+      this.height = 0;
+    }
+  }
+
+  up(): void {
+    this.height += 10000;
+  }
+
+  down(): void {
+    //
+  }
+
+  left(): void {
+    //
+  }
+
+  right(): void {
+    //
+  }
 }
